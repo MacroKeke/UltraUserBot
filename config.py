11e1b42c25148cd653e4bdf5fa81d9c4330ae2b8
MacroKeke <106157697+MacroKeke@users.noreply.github.com>
2022-06-07 11:37:49 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_HASH = os.getenv("026a7519311e9d8b85649d1eda64e362")
+API_ID = int(os.getenv("15415370"))
+HEROKU_API = os.getenv("c6ea0f23-9bc7-4296-b528-ca1d7cc638b4")
+HEROKU_APP_NAME = os.getenv("frknabiskr")
+PY_SESSION = os.getenv("BQB0GVSogqF_m0BBtkAgDDocYMa1CZqx84qw1q5PXWyZHb1WQ9XDUzG8TXiEjrrY3n2tJ8EdHhSg9zx-x6YXPUq7VtPcGbmCwYwsupvnNUsq-f7QMql4x7wqna6Zeg1NZx4bAFJlv78c7iT6AUCavm46aVjab-0UWokSjoNTHghWUTXZ7TaM17LO9V95SQGqrhREL52g1PsZfHPHh1-XFU9pY-4L6OQZGtw3HS0M1JOg8kNsyVtBNkL4AP1Ptmf5LZYgw__3gLa4kqj2PgIAlXNYK4qNcUl_k4RtyrNyBq0BqLcdQBobnjHMCjhdzcFci8HbjHmEMBxrXpPndq6yjVBwAAAAAUXI7JUA")
+PREFIX = os.environ.get("PREFIX", ".")
+LOG_CHAT = int(os.getenv("-1001771876516"))
